bba0cfc2191cdf7c686a7ba392f6b0b678b05909
Gracevie Nzumba <Gracevienzumba@gmail.com>
2024-01-15 16:50:25 +0200

    mise a jour

--- src/Objet.jsx
+++ src/Objet.jsx
@@ -13,7 +13,24 @@ export default function Objet() {
       image: 'https://picsum.photos/200/800?random=1322868385361891328',
     }
 
-    return
+    return (
+      <div className='flex p-4 mt-4 text-white'>
+        <div className=''>
+          <img className='h-18 w-24 rounded-full' src={MyTweet.author_avatar} alt="image" />
+        </div>
+        <div className='ml-6'>
+          <h2>{MyTweet.source} <small>{MyTweet.date}</small></h2>
+          <Paraph name={MyTweet.text} />
+          <img className='pt-2 rounded-3xl' src={MyTweet.image} alt="" />
+          <div className='flex pt-5 justify-between'>
+            <Myicoment src="src/assets/Reply.svg" name={MyTweet.replies} />
+            <Myicoment src="src/assets/Retweet.svg" name={MyTweet.retweets} />
+            <Myicoment src="src/assets/React.svg" name={MyTweet.favorites} />
+            <Myicoment src="src/assets/Share.svg" name="" />
+          </div>
+        </div>
+      </div>
+  )
   
 }
 
