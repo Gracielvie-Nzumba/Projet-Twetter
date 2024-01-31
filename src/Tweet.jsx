f865d7af75c264611f8ef76482c2daa6f784ad90
Gracevie Nzumba <Gracevienzumba@gmail.com>
2024-01-31 16:31:32 +0200

    commit1

--- src/Tweet.jsx
+++ src/Tweet.jsx
@@ -1,24 +1,48 @@
-export default function Tweet(props) {
+export default function Tweet({unTweet}) {
+
+  const objet =  {
+    author_avatar: 'https://picsum.photos/200?random=1609942590000',
+    source: 'Apple',
+    date: 1609942590000,
+    favorites: '396603',
+    id: '1346822958006886400',
+    isVerified: false,
+    replies: '896',
+    retweets: '54694',
+    text: 'Even Mexico uses Voter I.D.',
+    image: 'https://picsum.photos/1024/200?random=1346822958006886400',
+  }
+
   return (
-    <>
-      <div className="flex gap-4 p-2">
-        <img
-          className="rounded-full w-10 h-10"
-          src="Tweet/Body/pains-cmohamed-hassouna-1024x683.jpg"
-        />
-        <div>
-          <div className="flex">
-            <h2>{props.titre}</h2>
-            <img src="src/assets/Verified.svg" />
+   <>
+
+    <div className="flex gap-4">
+      <div>
+        <img src= {unTweet.author_avatar} className="rounded-full w-10 h-10"/>
+      </div>
+      <div className=" ml-4 mt-4">
+        <div className="flex pb-6">
+          <h2 className="font-bold">{unTweet.source}</h2>
+          <img src="src/assets/Verified.svg" alt="" className="w-7 h-7"/>
+        </div>
+        <p>{unTweet.text}</p>
+        {
+          unTweet.image && <img  src = {unTweet.image} className="w-full rounded-3xl object-cover h-60 "/>
+
+        }
+        <div className="flex gap-10 pb-7 pt-7 ">
+          <div className="">
+          <img src="src/assets/Reply.svg" alt=""/>
           </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
-            ipsam quis minima ducimus suscipit natus quia illo nesciunt quos,
-            aliquam eaque accusantium repudiandae, cum sit cumque blanditiis
-            nulla architecto impedit.
-          </p>
+          <span className="">0</span>
+          <img src="src/assets/Retweet.svg" alt="" />
+          <span>0</span>
+          <img src="src/assets/React.svg" alt="" />
+          <span>0</span>
+          <img src="src/assets/Share.svg" alt="" />
         </div>
       </div>
-    </>
+    </div>
+     </>
   );
 }
