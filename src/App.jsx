7ab5ab896aedd082b1f7310b294fd492dffa6a3a
Gracevie Nzumba <Gracevienzumba@gmail.com>
2024-02-01 13:07:55 +0200

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Route,
-  Routes,
-  Link,
-  BrowserRouter,
-} from 'react-router-dom';
+;
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
